1ff30462e8072019c1f88ee63870d9fb06d733bd
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2022-08-18 16:05:20 +0530

    queryselector, event listener, call back function in js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-console.log("this is from script file")
+var btnTranslate = document.querySelector("#btn-translate");
+console.log(btnTranslate)
 
-//promp to take input
-var username = prompt("what is your name?")
+function clickEventHandler(){
+    console.log("clicked!!")
+}
 
-//using alert for output
-alert("welcome to my app " + username)
+btnTranslate.addEventListener("click", clickEventHandler )
