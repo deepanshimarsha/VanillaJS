0a30ed62557fb7c90a75a138689c2c5507b43aee
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2022-08-19 19:52:30 +0530

    fetching api server

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ console.log(btnTranslate)
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector(".txt-output")
 
+var testserverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getRequestUrl(text){
+    return serverUrl + "?" + "text=" + text
+}
+
+
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("server responding with an error")
+}
 
 function clickEventHandler(){
-    console.log("clicked!!")
-    console.log(txtInput.value)
-    txtOutput.innerHTML = txtInput.value
+    var inputText = txtInput.value
+
+    fetch(getRequestUrl(inputText))
+    .then(Response => Response.json())
+    .then(json => {
+        txtOutput.innerText = json.contents.translated })
+    .catch(errorHandler)
+    
 }
 
 btnTranslate.addEventListener("click", clickEventHandler )
